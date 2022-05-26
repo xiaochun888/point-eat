2df9747ff2bd80fb826b83e4f3b252e49e9918b8
xiaochun888 <79648676+xiaochun888@users.noreply.github.com>
2022-05-26 14:20:16 +0200

    Point eat Site

---
+++ src/components/termsOfUse.vue
@@ -0,0 +1,4 @@
+<template>
+<div class="terms-of-use">
+</div>
+</template>
---
+++ test/unit/specs/home.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import home from '@/components/home';
+
+describe('home.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(home);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .toEqual('Welcome to Your Vue.js App');
+  });
+});
